babfebd8b1fbbbd4029decf0e3ac1a071c462609
Adam Greig <adam@adamgreig.com>
2015-12-29 01:26:14 +0000

    Add modreport.py

---
+++ scripts/modreport.py
@@ -0,0 +1,38 @@
+"""
+modreport.py
+Copyright 2015 Adam Greig
+
+Generate a report of all available footprints.
+"""
+
+import sys
+import os
+import glob
+
+import moddraw
+
+
+def main(prettypath, outpath):
+    imgpath = os.path.join(outpath, "img")
+    os.makedirs(imgpath, exist_ok=True)
+    mods = []
+
+    for f in glob.glob(os.path.join(prettypath, "*.kicad_mod")):
+        modname = os.path.splitext(os.path.basename(f))[0]
+        moddraw.main(f, os.path.join(imgpath, modname + ".png"))
+        mods.append(modname)
+
+    with open(os.path.join(outpath, "report.html"), "w") as f:
+        f.write("<!doctype html>\n")
+        f.write("<table border=1>\n")
+        for mod in mods:
+            f.write("<tr><td><a href=img/{}.png>".format(mod))
+            f.write("<img src=img/{}.png width=256 height=256>".format(mod))
+            f.write("</a></td><td>{}</td></tr>".format(mod))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        prettypath = sys.argv[1]
+        outpath = sys.argv[2]
+        main(prettypath, outpath)
