31d097fd4c434c8cf6ccd0a7060aaf52b7428b42
Adam Greig <adam@adamgreig.com>
2016-01-12 02:01:29 +0000

    add more connector symbols

--- scripts/connectorlib.py
+++ scripts/connectorlib.py
@@ -71,7 +71,7 @@ def main(libpath):
     f.write("# See github.com/adamgreig/agg-kicad\n")
     f.write("#=====================================================\n\n")
 
-    for pincount in range(1, 11):
+    for pincount in range(1, 17):
         onerow(f, pincount)
         tworow(f, pincount)
 
