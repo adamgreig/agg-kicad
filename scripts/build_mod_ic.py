a1f9a96df2b137eb714504e8cbd09ff439028e25
Adam Greig <adam@adamgreig.com>
2016-12-09 04:13:03 +0000

    Add CS2100-CP PLL

--- scripts/build_mod_ic.py
+++ scripts/build_mod_ic.py
@@ -174,6 +174,31 @@ config = {
         "ep_paste_shape": (1.83, 1.89, 0, 0),
         "ep_mask_shape": (1.83, 1.89, 0, 0),
         "ep_vias": (0.3, 0.5, 0.3),
+        "model": {
+            "path": "${KISYS3DMOD}/Housings_SSOP.3dshapes/MSOP-10-1EP_3x3mm_Pitch0.5mm.wrl",
+            "offset": (0, 0, 0),
+            "scale": (1, 1, 1),
+            "rotate": (0, 0, 0),
+        }
+    },
+
+    # MSOP-10 with 0.5mm pin pitch
+    # JEDEC MO-187
+    # IPC-7351B: SOP50P490X110-10N
+    "MSOP-10": {
+        "rows": 2,
+        "pins": 10,
+        "pin_pitch": 0.5,
+        "row_pitch": 4.4,
+        "pad_shape": (1.45, 0.3),
+        "chip_shape": (3.0, 3.0),
+        "pin_shape": (0.95, 0.25),
+        "model": {
+            "path": "${KISYS3DMOD}/Housings_SSOP.3dshapes/MSOP-10_3x3mm_Pitch0.5mm.wrl",
+            "offset": (0, 0, 0),
+            "scale": (1, 1, 1),
+            "rotate": (0, 0, 0),
+        }
     },
 
     # TSSOP-16 from JEDEC MO-153AB
