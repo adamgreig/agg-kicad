53de6c9a4f799009d022af46216e1427ee8d9ad1
Adam Greig <adam@adamgreig.com>
2022-07-15 17:51:10 +0100

    Port check_lib to kicad6 format

--- scripts/check_lib.py
+++ scripts/check_lib.py
@@ -1,170 +1,209 @@
 """
 check_lib.py
-Copyright 2015 Adam Greig
+Copyright 2015-2022 Adam Greig
 Licensed under the MIT licence, see LICENSE file for details.
 
 Check all library files in a directory against a set of consistency rules.
 """
-
-from __future__ import print_function, division
 
 import sys
 import os
 import fnmatch
 import re
 import argparse
+import sexp
 
 
-EXCLUSIONS = ("agg-kicad.lib", "conn.lib", "power.lib", "switch.lib",
-              "tec2.lib", "tel10.lib", "siz340dt.lib")
+EXCLUDE = {
+    'one_symbol': [],
+    'pin_grid': [],
+    'pin_length': [],
+    'pin_font': [],
+    'missing_pins': [],
+    'duplicate_pins': [],
+    'missing_box': [],
+    'invisible_ref': [],
+    'invisible_val': [],
+    'property_horizontal': [],
+    'property_font': [],
+    'ref_above_val': [],
+}
 
 
-re_defs = re.compile("^DEF (?P<name>[^ ]*) (?P<des>[^ ]*) ", re.MULTILINE)
-re_pins = re.compile("^X (?P<name>[^ ]*) (?P<num>[^ ]*)"
-                     " (?P<x>[0-9\-]*) (?P<y>[0-9\-]*) (?P<len>[0-9]*)"
-                     " [A-Z] (?P<numsize>[0-9]*) (?P<namesize>[0-9]*)",
-                     re.MULTILINE)
-re_refn = re.compile("^F0 (?P<value>[^ ]*) (?P<x>[0-9\-]*) (?P<y>[0-9\-]*)"
-                     " (?P<size>[0-9]*) (?P<orient>[VH]) (?P<visible>[IV])"
-                     " (?P<hjust>[LRC]) (?P<vjust>[TBC]{1,3})", re.MULTILINE)
-re_name = re.compile("^F1 (?P<value>[^ ]*) (?P<x>[0-9\-]*) (?P<y>[0-9\-]*)"
-                     " (?P<size>[0-9]*) (?P<orient>[VH]) (?P<visible>[IV])"
-                     " (?P<hjust>[LRC]) (?P<vjust>[TBC]{1,3})", re.MULTILINE)
-re_fp = re.compile("^F2 (?P<value>[^ ]*) (?P<x>[0-9\-]*) (?P<y>[0-9\-]*)"
-                   " (?P<size>[0-9]*) (?P<orient>[VH]) (?P<visible>[IV])"
-                   " (?P<hjust>[LRC]) (?P<vjust>[TBC]{1,3})", re.MULTILINE)
-re_ds = re.compile("^F3 (?P<value>[^ ]*) (?P<x>[0-9\-]*) (?P<y>[0-9\-]*)"
-                   " (?P<size>[0-9]*) (?P<orient>[VH]) (?P<visible>[IV])"
-                   " (?P<hjust>[LRC]) (?P<vjust>[TBC]{1,3})", re.MULTILINE)
-re_oc = re.compile("^F[4-9] (?P<value>[^ ]*) (?P<x>[0-9\-]*) (?P<y>[0-9\-]*)"
-                   " (?P<size>[0-9]*) (?P<orient>[VH]) (?P<visible>[IV])"
-                   " (?P<hjust>[LRC]) (?P<vjust>[TBC]{1,3})", re.MULTILINE)
-re_poly = re.compile("^[SP] .* (?P<fill>[NfF])$", re.MULTILINE)
+def excludes(libf):
+    name = os.path.basename(libf)
+    return [k for k in EXCLUDE if name in EXCLUDE[k]]
 
 
-def checkdefs(contents, libf, errs):
-
-    # Check there's only one symbol in the library
-    n_defs = re_defs.findall(contents)
-    if len(n_defs) > 1:
-        errs.append("Found more than one component in library")
-    elif len(n_defs) == 0:
-        errs.append("Did not find any components in library")
-
-    # Check symbol name matches library name
-    partname = n_defs[0][0]
-    designator = n_defs[0][1]
-    libname = os.path.split(libf)[-1].split(".")[0]
-    if partname.lower() != libname:
-        errs.append("Part name '{}' does not match library name '{}'"
-                    .format(partname, libname))
-
-    return partname, designator
+class Property:
+    def __init__(self, cfg):
+        self.cfg = cfg
+        self.key = cfg[1]
+        self.val = cfg[2]
+        self.effects = [p for p in cfg if p[0] == 'effects'][0]
+        self.font = [p for p in self.effects if p[0] == 'font'][0]
+        self.font_size = [(float(p[1]), float(p[2])) for p in self.font
+                          if p[0] == 'size'][0]
+        self.hidden = any(p == 'hide' for p in self.effects)
+        if any(p[0] == 'at' for p in cfg):
+            self.at = [p[1:] for p in cfg if p[0] == 'at'][0]
+            self.x = float(self.at[0])
+            self.y = float(self.at[1])
+            self.rot = float(self.at[2])
 
 
-def checkpins(contents, designator, errs):
-    pins = re_pins.findall(contents)
+
+class Pin:
+    def __init__(self, cfg):
+        assert cfg[0] == 'pin'
+        self.cfg = cfg
+        self.etype = cfg[1]
+        self.style = cfg[2]
+        self.at = [c[1:] for c in cfg if c[0] == 'at'][0]
+        self.x = float(self.at[0])
+        self.y = float(self.at[1])
+        self.rot = float(self.at[2])
+        self.length = [float(c[1]) for c in cfg if c[0] == 'length'][0]
+        self.prop_name = [Property(c) for c in cfg if c[0] == 'name'][0]
+        self.prop_num = [Property(c) for c in cfg if c[0] == 'number'][0]
+        self.name = self.prop_name.key
+        self.num = self.prop_num.key
+
+
+class Symbol:
+    def __init__(self, cfg):
+        assert cfg[0] == 'symbol'
+        self.cfg = cfg
+        self.name = cfg[1]
+        self.props = [Property(p) for p in cfg if p[0] == 'property']
+        self.prop_ref = [p for p in self.props if p.key == 'Reference'][0]
+        self.prop_val = [p for p in self.props if p.key == 'Value'][0]
+        self.prop_fp  = [p for p in self.props if p.key == 'Footprint'][0]
+        self.ref = self.prop_ref.val
+        self.val = self.prop_val.val
+        self.fp = self.prop_fp.val
+        self.pins = [Pin(p) for p in cfg if p[0] == 'pin']
+        self.polys = [p for p in cfg if p[0] == 'polyline']
+        self.rects = [p for p in cfg if p[0] == 'rectangle']
+        for subsym in [s for s in cfg if s[0] == 'symbol']:
+            self.pins += [Pin(p) for p in subsym if p[0] == 'pin']
+            self.polys += [p for p in subsym if p[0] == 'polyline']
+            self.rects += [p for p in subsym if p[0] == 'rectangle']
+
+
+def is_multiple(n, m):
+    a = n % m
+    return a < 1e-10 or (m - a) < 1e-10
+
+
+def check_symbols(symbols, libf, exclusions, errs):
+    if 'one_symbol' not in exclusions:
+        if len(symbols) > 1:
+            errs.append(f"Found more than one symbol in library {libf}")
+        elif len(symbols) == 0:
+            errs.append(f"Did not find any symbols in library {libf}")
+        symbol = symbols[0]
+        libname = os.path.splitext(os.path.basename(libf))[0].lower()
+        if symbol.name.lower() != libname:
+            errs.append(f"Symbol name {symbol.name} does not match library {libf}")
+        if symbol.val.lower() != symbol.name.lower():
+            errs.append(f"Symbol value {symbol.val} does not match name {symbol.name}")
+
+
+def check_pins(symbol, exclusions, errs):
     nums = []
     nums_numeric = []
-    for name, num, x, y, length, numsize, namesize in pins:
+    for pin in symbol.pins:
         # Check pins lie on 100mil grid
-        if int(x) % 100 != 0 or int(y) % 100 != 0:
-            errs.append("Pin '{}' not on 100mil grid".format(name))
-        # Check pins in IC and U parts are 100mil long
-        if designator in ("IC", "U") and int(length) not in (100, 150):
-            errs.append("Pin '{}' not 100 or 150mil long, but part is IC or U"
-                        .format(name))
-        # Check pin text fields are 50mil sized
-        if int(namesize) != 50 or (int(numsize) != 50 and num.isdigit()):
-            errs.append("Pin '{}' font size not 50mil".format(name))
-        # Collect numeric pins
-        nums.append(num)
-        if num.isdigit():
-            nums_numeric.append(int(num))
+        if 'pin_grid' not in exclusions:
+            if not is_multiple(pin.x, 2.54) or not is_multiple(pin.y, 2.54):
+                errs.append(f"Pin {pin.name} not on 100mil grid")
+        if 'pin_length' not in exclusions:
+            if symbol.ref in ("IC", "U") and pin.length not in (2.54, 3.81):
+                errs.append(f"Pin {pin.name} not 100 or 150mil long")
+        if 'pin_font' not in exclusions:
+            if pin.prop_name.font_size != (1.27, 1.27):
+                errs.append(f"Pin {pin.name} font not 50mil")
+            if pin.num.isdigit() and pin.prop_num.font_size != (1.27, 1.27):
+                errs.append(f"Pin {pin.name} font not 50mil")
+        nums.append(pin.num)
+        if pin.num.isdigit():
+            nums_numeric.append(int(pin.num))
 
-    if nums_numeric:
+    if 'missing_pins' not in exclusions and nums_numeric:
         expected = set(range(min(nums_numeric), max(nums_numeric)+1))
         if set(nums_numeric) != expected:
             missing = [str(x) for x in set(expected) - set(nums_numeric)]
-            errs.append("Missing pins {}".format(", ".join(missing)))
+            errs.append(f"{sym.name} missing pins {missing}")
 
-    if nums:
+    if 'duplicate_pins' not in exclusions and nums:
         duplicates = set([str(x) for x in nums if nums.count(x) > 1])
         if duplicates:
-            errs.append("Duplicated pins {}".format(", ".join(duplicates)))
+            errs.append(f"{sym.name} has duplicate pins {duplicates}")
 
 
-def checkboxes(contents, designator, errs):
-    if designator == "IC":
-        boxes = re_poly.findall(contents)
-        if "f" not in boxes:
+
+def check_drawings(symbol, exclusions, errs):
+    if symbol.val == "IC" and 'missing_box' not in exclusions:
+        got_box = False
+        for rect in symbol.rects:
+            for p in rect:
+                if p[0] == 'fill':
+                    for q in p:
+                        if q[0] == 'type':
+                            if q[1] == "background":
+                                got_box = True
+        if not got_box:
             errs.append("No background-filled box/poly found, but part is IC")
 
 
-def checkfields(contents, errs, prettypath):
-    refn_f = re_refn.findall(contents)
-    name_f = re_name.findall(contents)
-    foot_f = re_fp.findall(contents)
-    data_f = re_ds.findall(contents)
-    code_f = re_oc.findall(contents)
-
-    fields = ((refn_f, "reference"), (name_f, "name"), (foot_f, "footprint"),
-              (data_f, "datasheet"), (code_f, "order code"))
-
-    for field, fn in fields:
-        for value, x, y, size, orient, visible, hjust, vjust in field:
-            if fn in ("reference", "name"):
-                if visible != "V":
-                    if "#invisible{}".format(fn) not in contents:
-                        errs.append("Field {} not visible".format(fn))
-            else:
-                if visible != "I":
-                    errs.append("Field {} visible".format(fn))
-            if orient != "H":
-                errs.append("Field {} not horizontal".format(fn))
-            if size != "50":
-                errs.append("Field {} font size not 50".format(fn))
-
-    refn_y = int(refn_f[0][2])
-    name_y = int(name_f[0][2])
-
-    if refn_y <= name_y:
-        errs.append("Component reference not above component name")
-
-    fp = foot_f[0][0][1:-1]
-    if fp.startswith("agg:"):
-        fp = fp.split(":")[1] + ".kicad_mod"
+def check_fields(symbol, exclusions, errs, prettypath):
+    if 'invisible_ref' not in exclusions and symbol.prop_ref.hidden:
+        errs.append(f"{symbol.name} reference field hidden")
+    if 'invisible_val' not in exclusions and symbol.prop_val.hidden:
+        errs.append(f"{symbol.name} value field hidden")
+    if 'property_horizontal' not in exclusions:
+        if any(p.rot != 0 for p in symbol.props):
+            errs.append(f"{symbol.name} field not horizontal")
+    if 'property_font' not in exclusions:
+        if any(p.font_size != (1.27, 1.27) for p in symbol.props):
+            errs.append(f"{symbol.name} field font size not 50mil")
+    if 'ref_above_val' not in exclusions:
+        if symbol.prop_ref.y <= symbol.prop_val.y:
+            errs.append(f"{symbol.name} reference not above value")
+    if symbol.fp.startswith("agg:"):
+        fp = symbol.fp.split(":")[1] + ".kicad_mod"
         path = os.path.join(prettypath, fp)
         if not os.path.exists(path):
-            errs.append("Component references non-existent footprint {}"
-                        .format(fp))
+            errs.append(f"Component references non-existant footprint {fp}")
     elif len(fp) > 0 and ":" not in fp:
-        errs.append("Footprint '{}' does not specify a library name"
-                    .format(fp))
+        errs.append(f"Footprint {fp} doesn't specify a library name")
 
 
 def checklib(libf, prettypath, verbose=False):
     errs = []
-
-    # Check if there's a corresponding .dcm file
-    dcmpath = ".".join(libf.split(".")[:-1]) + ".dcm"
-    if not os.path.isfile(dcmpath):
-        errs.append("No corresponding DCM found")
+    exclusions = excludes(libf)
 
     with open(libf) as f:
-        contents = f.read()
+        contents = sexp.parse(f.read())
 
-    # Check there's only one symbol and its name matches the library file
-    partname, designator = checkdefs(contents, libf, errs)
+    symbols = []
+    for node in contents:
+        if node[0] == 'symbol':
+            symbols.append(Symbol(node))
 
-    # Check pins
-    checkpins(contents, designator, errs)
+    check_symbols(symbols, libf, exclusions, errs)
 
-    # If part is an IC check at least one filled box/polyline is present
-    checkboxes(contents, designator, errs)
+    for symbol in symbols:
 
-    # Check fields
-    checkfields(contents, errs, prettypath)
+        # Check pins
+        check_pins(symbol, exclusions, errs)
+
+        # If part is an IC check at least one filled box/polyline is present
+        check_drawings(symbol, exclusions, errs)
+
+        # Check fields
+        check_fields(symbol, exclusions, errs, prettypath)
 
     if len(errs) == 0:
         if verbose:
@@ -183,14 +222,11 @@ def main(libpath, prettypath, verbose=False):
     for dirpath, dirnames, files in os.walk(libpath):
         dirnames.sort()
         files.sort()
-        for f in fnmatch.filter(files, "*.lib"):
+        for f in fnmatch.filter(files, "*.kicad_sym"):
             path = os.path.join(dirpath, f)
-            if f not in EXCLUSIONS:
-                result = checklib(path, prettypath, verbose)
-                if not result:
-                    ok = False
-            elif verbose:
-                print("Skipping '{}'".format(path))
+            result = checklib(path, prettypath, verbose)
+            if not result:
+                ok = False
     return ok
 
 if __name__ == "__main__":
